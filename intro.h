67cbf1abf4ddb3ebd23b6b671b5095196a24dcd2
cmomodo <ceesay.ml@outlook.com>
2021-07-28 11:40:23 +0100

    the room file has been added

---
+++ intro.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Intro{
+    public:
+    void hello(){cout <<"This will be a text based Game.";}
+};
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "intro.h"
+#include "room.h"
+
+int main() {
+
+    Intro wel;
+    wel.hello();
+}
