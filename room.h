e62ae35d82226b48c3751533df00f4f61ba8bee9
cmomodo <ceesay.ml@outlook.com>
2021-08-03 18:12:07 +0100

    setting up the rooom now

--- room.h
+++ room.h
@@ -3,9 +3,10 @@
 #include <vector>
 #include <algorithm>
 
-class Room{
-    private:
-    string h;
-    int 
+void displayRoom(const vector<char>& board);
 
-};
+void displayRoom(const vector<char>& board)
+{
+    cout << "\n\t" <<board[0] << " | " << board[1] << " | " << board[2];
+    cout << "\n\t" << "Open the Dooor";
+}
