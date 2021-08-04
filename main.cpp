16699b1744b62447875f56031b14636b66c15124
cmomodo <ceesay.ml@outlook.com>
2021-08-04 15:30:33 +0100

    Will run all the h files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
 #include "intro.h"
 #include "room.h"
+#include "pmove.h"
+#include "askOption.h"
+
+const char Right = 'R';
+const char Left = 'L';
+const char EMPTY = ' ';
+const char Straight = 'S';
+const char TIE = 'T';
+    const char NO_ONE = 'N';;
+
+void displayRoom(const vector<char>& board);
 
 int main() {
 
     Intro wel;
     wel.hello();
+    const int NUM_WAYS = 8;
+    vector<char> board(NUM_WAYS, EMPTY);
+    
+
+    displayRoom(board);
+
+    return 0;
 }
